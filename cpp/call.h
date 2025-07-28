f0484a88ad0ee7286e31985ac882038b49928a49
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-07-28 13:42:33 +0200

    test ing

--- cpp/call.h
+++ cpp/call.h
@@ -13,11 +13,11 @@
 
 #define CALLTYPE __cdecl
 #define API_DLL __declspec( dllimport ) 
-
+/*
 API_DLL IMyExport* CALLTYPE CreateMyExportInstance();
 API_DLL unsigned CALLTYPE MyExport_GetCurrentUserId( IMyExport* ptr );
 API_DLL void CALLTYPE MyExport_Start( IMyExport* ptr );
 API_DLL void CALLTYPE MyExport_Stop( IMyExport* ptr) ;
 API_DLL void CALLTYPE DestroyMyExportInstance( IMyExport* ptr );
-
+*/
 #endif // CALL_H

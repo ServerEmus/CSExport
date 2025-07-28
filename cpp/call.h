288f121b28f1c0a1234e32e473ec38c6273d37fb
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-07-28 13:23:28 +0200

    i hate c++

--- cpp/call.h
+++ cpp/call.h
@@ -1,3 +1,5 @@
+#ifndef CALL_H
+#define CALL_H
 #pragma once
 #include "test.h"
 #include <string.h>
@@ -15,3 +17,5 @@ API_DLL unsigned CALLTYPE MyExport_GetCurrentUserId( IMyExport* ptr );
 API_DLL void CALLTYPE MyExport_Start( IMyExport* ptr );
 API_DLL void CALLTYPE MyExport_Stop( IMyExport* ptr) ;
 API_DLL void CALLTYPE DestroyMyExportInstance( IMyExport* ptr );
+
+#endif // CALL_H
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "test.h"
-#include "call.h"
+//#include "test.h"
+//#include "call.h"
 
 int main() 
 {
